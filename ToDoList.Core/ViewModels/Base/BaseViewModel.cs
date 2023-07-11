8fe0d8ed7700bccf3cb8cceedda1d9240e40fb99
Cebix90 <cebulmat@gmail.com>
2023-07-11 12:20:26 +0200

    Added LoginPage

--- ToDoList.Core/ViewModels/Base/BaseViewModel.cs
+++ ToDoList.Core/ViewModels/Base/BaseViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using System.ComponentModel;
+using ToDoList.Database;
 
 namespace ToDoList.Core.Models.Base;
 
-public class BaseViewModel :INotifyPropertyChanged
+public class BaseViewModel : INotifyPropertyChanged
 {
-    public event PropertyChangedEventHandler PropertyChanged = (s , e) => { };
+    public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
 
     protected void OnPropertyChanged(string propertyName)
     {
         PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    public bool AuthenticateUser(string email, string password, ToDoListDbContext context)
+    {
+        bool isAuthenticated = context.Users.Any(u => u.Email == email && u.Password == password);
+        return isAuthenticated;
+    }
+
+
 }
--- ToDoList.Core/ViewModels/Pages/WorkTasksPageViewModel.cs
+++ ToDoList.Core/ViewModels/Pages/WorkTasksPageViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using ToDoList.Core.Helpers;
 using ToDoList.Core.Models.Base;
